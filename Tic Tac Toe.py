3b7a0cab32d3fd950aa39e96e98e0beeab8c4c87
Arxalier <118110881+Arxalier@users.noreply.github.com>
2022-11-14 17:09:10 +0530

    Update Tic Tac Toe.py
    Changed to 6 lines only

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,11 +1,6 @@
-c1=c2=c3=c4=c5=c6=c7=c8=c9=' '
-tiles, player, start=[c1,c2,c3,c4,c5,c6,c7,c8,c9], True, ["O","X"]
-while True:
-    print("Tic Tac Toe\n1, 2, 3\n4, 5, 6\n7, 8, 9\nRefer to the numbers to play a move\n"+"|".join(tiles[0:3])+"\n"+"-"*6+"\n"+"|".join(tiles[3:6])+"\n"+"-"*6+"\n"+"|".join(tiles[6:])+"\n"+"You are X") if player else print("Tic Tac Toe\n1, 2, 3\n4, 5, 6\n7, 8, 9\nRefer to the numbers to play a move\n"+"|".join(tiles[0:3])+"\n"+"-"*6+"\n"+"|".join(tiles[3:6])+"\n"+"-"*6+"\n"+"|".join(tiles[6:])+"\n"+"You are O")
-    tile, player = input("Enter tile to place in: "), not player
-    tiles[int(tile)-1]=start[int(not player)]
-    if (tiles[0]==tiles[1]==tiles[2] and tiles[0]!=" ")or(tiles[3]==tiles[4]==tiles[5] and tiles[3]!=" ")or(tiles[6]==tiles[7]==tiles[8] and tiles[6]!=" ")or(tiles[0]==tiles[4]==tiles[8] and tiles[0]!=" ")or(tiles[2]==tiles[4]==tiles[6] and tiles[2]!=" ")or(tiles[0]==tiles[3]==tiles[6] and tiles[0]!=" ")or(tiles[1]==tiles[4]==tiles[7] and tiles[1]!=" ")or(tiles[2]==tiles[5]==tiles[8] and tiles[2]!=" "):
-        print(start[int(not player)], "wins!\n"+"|".join(tiles[0:3]) + "\n" + "-" * 6 + "\n" + "|".join(tiles[3:6]) + "\n" + "-" * 6 + "\n" + "|".join(tiles[6:]) + "\n")
-        break
-
-
+tiles,player,start,play=[' ' for i in range(9)],False,["O","X"],True # TIC TAC TOE IN 6 LINES IN PYTHON, player = False for x to play and player = True for y to play
+while play:
+    print("Tic Tac Toe\n1, 2, 3\n4, 5, 6\n7, 8, 9\nRefer to the numbers to play a move\n"+"|".join(tiles[0:3])+"\n"+"—"*6+"\n"+"|".join(tiles[3:6])+"\n"+"—"*6+"\n"+"|".join(tiles[6:])+"\n"+"You are O") if player else print("Tic Tac Toe\n1, 2, 3\n4, 5, 6\n7, 8, 9\nRefer to the numbers to play a move\n"+"|".join(tiles[0:3])+"\n"+"—"*6+"\n"+"|".join(tiles[3:6])+"\n"+"—"*6+"\n"+"|".join(tiles[6:])+"\n"+"You are X")
+    tile,player,tiles[int(tile)-1]=input("Enter tile to place in: "),not player,start[int(not player)]
+    if ((tiles[0]==tiles[1]==tiles[2] and tiles[0]!=" ")or(tiles[3]==tiles[4]==tiles[5] and tiles[3]!=" ")or(tiles[6]==tiles[7]==tiles[8] and tiles[6]!=" ")or(tiles[0]==tiles[4]==tiles[8] and tiles[0]!=" ")or(tiles[2]==tiles[4]==tiles[6] and tiles[2]!=" ")or(tiles[0]==tiles[3]==tiles[6] and tiles[0]!=" ")or(tiles[1]==tiles[4]==tiles[7] and tiles[1]!=" ")or(tiles[2]==tiles[5]==tiles[8] and tiles[2]!=" ")): play = False
+print(start[int(player)], "wins!\n"+"|".join(tiles[0:3]) + "\n" + "—" * 6 + "\n" + "|".join(tiles[3:6]) + "\n" + "—" * 6 + "\n" + "|".join(tiles[6:]) + "\n")
